d36ac0c7553c5c0e4b67047f3acd9ecdc57a4afb
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-06-05 13:24:04 +0530

    Update Dynamic_Editor_System.java

--- Dynamic_Editor_System.java
+++ Dynamic_Editor_System.java
@@ -16,3 +16,44 @@ Delete: Java
 Replace: Programming  - Coding
 Reverse final string
 */
+import java.util.Scanner;
+
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        char firstNonRepeat = ' ';
+        boolean found = false;
+
+        
+        for (int i = 0; i < str.length(); i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                firstNonRepeat = str.charAt(i);
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("First Non-Repeating Character: " + firstNonRepeat);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+    }
+}
