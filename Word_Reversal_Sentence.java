818085f2eee87e3c381547138c323aae102100c4
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-06-05 13:30:17 +0530

    Update Word_Reversal_Sentence.java

--- Word_Reversal_Sentence.java
+++ Word_Reversal_Sentence.java
@@ -8,4 +8,33 @@ Reverses each word, but keeps word order same
 Example:
 Input: Java is fun
 Output: avaJ si nuf
-  */
+  */import java.util.Scanner;
+
+public class WordReversalInSentence {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+     
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        
+        String[] words = sentence.split(" ");
+
+        System.out.print("Output: ");
+
+        
+        for (int i = 0; i < words.length; i++) {
+
+            String reverse = "";
+
+            for (int j = words[i].length() - 1; j >= 0; j--) {
+                reverse = reverse + words[i].charAt(j);
+            }
+
+            System.out.print(reverse + " ");
+        }
+    }
+}
