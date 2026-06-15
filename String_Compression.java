a0b648d96bad067413d2b489cf8e6db0dae29d0b
cse25bcsh96-hue <cse.25bcsh96@silicon.ac.in>
2026-06-15 23:32:21 +0530

    Update String_Compression.java

--- String_Compression.java
+++ String_Compression.java
@@ -1,10 +1,33 @@
-/*
-String Compression (Very Tricky)
+package JAVA_DAY_9;
 
-Create a program that:
+import java.util.Scanner;
 
-Compresses repeating characters
-Example:
-Input: aaabbcc
-Output: a3b2c2
-  */
+public class STRING_COMPRESSION {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Enter the string:");
+	        String string = sc.nextLine();
+
+	        int count = 1;
+	        System.out.println("Compressed string:");
+	        for(int i = 0; i < string.length(); i++)
+	        {
+	            while(i < string.length() - 1 &&
+	                  string.charAt(i) == string.charAt(i + 1))
+	            {
+	                count++;
+	                i++;
+	            }
+
+	            System.out.print(string.charAt(i));
+	            System.out.print(count);
+
+	            count = 1;
+	        }
+
+	        sc.close();
+	    }
+}
