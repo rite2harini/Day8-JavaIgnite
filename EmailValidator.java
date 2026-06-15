b9f016a85320f7cdf8bd144e2aabed0a28e77ea8
cse25bcsh96-hue <cse.25bcsh96@silicon.ac.in>
2026-06-15 23:29:54 +0530

    Add email validation in EmailValidator class
    Implement email validation logic to check for '@' and '.' characters.

--- EmailValidator.java
+++ EmailValidator.java
@@ -1,15 +1,30 @@
-/*
-Create a program that:
+package JAVA_DAY_9;
 
-Takes a string input
+import java.util.Scanner;
 
-Print:
+public class EMAIL_VALIDATOR {
 
-Uppercase version
-Lowercase version
-Example:
-Input: Java
-Output:
-JAVA
-java
-*/
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the email:");
+			String string = sc.nextLine();
+			for(int i=0;i<string.length();i++)
+			{
+				if(string.charAt(i)=='@')
+				{
+					for(int j=0;j<string.length();j++)
+					{
+						if(string.charAt(j)=='.')
+						{
+							System.out.println("Valid email! Proceed!");
+							return;
+						}
+					}
+				}
+			}
+			System.out.println("Invalid email!");
+			sc.close();
+		}
+	}
+}
