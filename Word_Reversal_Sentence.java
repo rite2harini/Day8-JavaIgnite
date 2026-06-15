b625ab7ff3413487508be34b8d497af7f9c24fc1
cse25bcsh96-hue <cse.25bcsh96@silicon.ac.in>
2026-06-15 23:33:43 +0530

    Add word reversal functionality in a sentence
    Implemented a program to reverse each word in a sentence while maintaining the original word order.

--- Word_Reversal_Sentence.java
+++ Word_Reversal_Sentence.java
@@ -1,11 +1,42 @@
-/*
-Word Reversal in Sentence
+package JAVA_DAY_9;
 
-Create a program that:
+import java.util.Scanner;
 
-Takes a full sentence as input
-Reverses each word, but keeps word order same
-Example:
-Input: Java is fun
-Output: avaJ si nuf
-  */
+public class WORD_REVERSAL_SENTENCE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Enter the sentence:");
+	        String str = sc.nextLine();
+
+	        String word = "";
+	        String result = "";
+
+	        for (int i = 0; i < str.length(); i++) {
+
+	            if (str.charAt(i) != ' ') {
+	                word += str.charAt(i);
+	            } else {
+
+	                // Reverse current word
+	                for (int j = word.length() - 1; j >= 0; j--) {
+	                    result += word.charAt(j);
+	                }
+
+	                result += " ";
+	                word = "";
+	            }
+	        }
+
+	        // Reverse and add last word
+	        for (int j = word.length() - 1; j >= 0; j--) {
+	            result += word.charAt(j);
+	        }
+
+	        System.out.println("Result: " + result);
+
+	        sc.close();
+	    }
+	}
